4a990390ec88fb5bdea2e10ed82a9c6a7fbb4663
rajatjain9 <rajat.jain@raspect.co>
2018-09-22 10:21:11 +0530

    Renamed team page

--- App.py
+++ App.py
@@ -2,9 +2,19 @@ import logging
 from codeitsuisse import  app
 logger = logging.getLogger(__name__)
 
+def get_primes(n):
+    numbers = set(range(n, 1, -1))
+    primes = []
+    while numbers:
+        p = numbers.pop()
+        primes.append(p)
+        numbers.difference_update(set(range(p*2, n+1, p)))
+    return primes
+
+
 @app.route('/', methods=['GET'])
 def default_route():
-    return "Team XYZ page";
+    return "Team typhoont10 page";
 
 if __name__ == "__main__":
     logFormatter = logging.Formatter("%(asctime)s [%(filename)s] [%(funcName)s] [%(lineno)d] [%(levelname)-5.5s]  %(message)s")
@@ -26,3 +36,12 @@ if __name__ == "__main__":
 @app.route('/hello', methods=['GET'])
 def say_hello():
 	return "HELLO, how are you?";
+
+@app.route('/prime-sum', methods=['POST'])
+def prime_sum:
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    inputValue = data.get("input");
+    
+    
+    
